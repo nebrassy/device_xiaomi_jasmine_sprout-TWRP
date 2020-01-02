53b4efd194c17b88c8b68ce9b75ec52bee66c38a
Max Weffers <rcstar6696@gmail.com>
2020-01-02 14:30:53 +0100

    jasmine: Treewide: Cleanup and Reorder
    * Remove Double Definition
    * Reordeder Alphabetic
    * Move Audio and Media Files to Configs
    * Move sensor files to configs
    * Reorder Properties
    * Fix whitespaces
    
    Change-Id: I68768418eef352f42467b88627280b05b2be40fe

--- aicp_jasmine_sprout.mk
+++ aicp_jasmine_sprout.mk
@@ -35,6 +35,10 @@ $(call inherit-product, vendor/aicp/config/common_full_phone.mk)
 # Define first api level
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
+# Build Fingerprint
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="jasmine-user 9 PKQ1.180904.001 V10.0.9.0.PDIMIXM release-keys"
+
 # Device identifier
 PRODUCT_BRAND := xiaomi
 PRODUCT_MANUFACTURER := Xiaomi
